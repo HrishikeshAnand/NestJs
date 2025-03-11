b1039f6026c3c4669c7d7accf4c5624a948510a0
Hrishikesh <hrishikeshanand3@gmail.com>
2025-03-11 14:08:13 +0530

    completed integrating typescript with typeOrm in nest

---
+++ lesson_02/src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+
+@Module({
+  imports: [UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'Hrishikesh12',
+      database: 'translense',
+      entities: [User],
+      synchronize: true
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
